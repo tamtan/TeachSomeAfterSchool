9235c652727eaf9fc818914398f9841ffa579c11
Tam Tan <vinhtam92@gmail.com>
2015-10-07 17:28:57 +0700

    - class info activity.

--- app/src/main/java/com/example/pc/teachsomeafterschool/Class/ClassInfoActivity.java
+++ app/src/main/java/com/example/pc/teachsomeafterschool/Class/ClassInfoActivity.java
@@ -17,12 +17,9 @@ import com.example.pc.teachsomeafterschool.Model.WeekSchedule;
 import com.example.pc.teachsomeafterschool.R;
 
 import org.androidannotations.annotations.Click;
-import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by TAM on 05-Oct-15.
@@ -50,7 +47,8 @@ public class ClassInfoActivity extends Activity {
     String weekTime;
     WeekSchedule weedSchedule;
     ClassModel classModel;
-    WeekDay tempWeekDay;
+    WeekDay chosenDay;
+    boolean isStartFinish, isStopFinish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,13 +61,118 @@ public class ClassInfoActivity extends Activity {
     }
 
     @Click({R.id.tvMonday, R.id.tvTuesday, R.id.tvWednesday, R.id.tvThursday, R.id.tvFriday, R.id.tvSaturday, R.id.tvSunday})
-    void AddNewWeekDay() {
-        Log.d("Start", start);
-        tvStartTime.setText(start);
-        tvStopTime.setText(stop);
+    void AddNewWeekDay(View view) {
+        //Log.d("Start", start);
+        chosenDay = new WeekDay();
+        isStartFinish = false;
+        isStopFinish = false;
+        tvStartTime.setText("Start");
+        tvStopTime.setText("Stop");
         if (tvStartTime.getVisibility() == View.INVISIBLE) {
             tvStartTime.setVisibility(View.VISIBLE);
             tvStopTime.setVisibility(View.VISIBLE);
+            tpTimePicker.setVisibility(View.VISIBLE);
+        }
+        switch (view.getId()) {
+            case R.id.tvMonday:
+                chosenDay.setDay("2");
+
+                break;
+            case R.id.tvTuesday:
+                chosenDay.setDay("3");
+
+                break;
+            case R.id.tvWednesday:
+                chosenDay.setDay("4");
+
+                break;
+            case R.id.tvThursday:
+                chosenDay.setDay("5");
+
+                break;
+            case R.id.tvFriday:
+                chosenDay.setDay("6");
+
+                break;
+            case R.id.tvSaturday:
+                chosenDay.setDay("7");
+
+                break;
+            case R.id.tvSunday:
+                chosenDay.setDay("CN");
+
+                break;
+        }
+
+    }
+
+    @Click(R.id.tvStartTime)
+    void AddNewStartTime() {
+        isStartFinish = false;
+        tvStartTime.setText("Start");
+//        int hour = tpTimePicker.getCurrentHour();
+//        int minute = tpTimePicker.getCurrentMinute();
+//        String time = Integer.toString(hour) +":"+ Integer.toString(minute);
+//        tvStartTime.setText(time);
+        if (tpTimePicker.getVisibility() == View.INVISIBLE) {
+            tpTimePicker.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Click(R.id.tvStopTime)
+    void AddNewStopTime() {
+        tvStopTime.setText("Stop");
+        isStopFinish = false;
+        if (tpTimePicker.getVisibility() == View.INVISIBLE) {
+            tpTimePicker.setVisibility(View.VISIBLE);
+
+        }
+    }
+
+    @Click(R.id.btOk)
+    void AddTime() {
+        if (!isStartFinish) {
+            int hour = tpTimePicker.getCurrentHour();
+            int minute = tpTimePicker.getCurrentMinute();
+            String time = Integer.toString(hour) + "-" + Integer.toString(minute);
+            tvStartTime.setText(time);
+            isStartFinish = true;
+        } else if (!isStopFinish) {
+            int hour = tpTimePicker.getCurrentHour();
+            int minute = tpTimePicker.getCurrentMinute();
+            String time = Integer.toString(hour) + ":" + Integer.toString(minute);
+            tvStopTime.setText(time);
+            isStopFinish = true;
+        } else {
+            if(!isDayExisted(chosenDay,weekDays)){
+                Add(chosenDay,weekDays);
+            }else{
+                Edit(chosenDay,weekDays);
+            }
+            if (tpTimePicker.getVisibility() == View.VISIBLE) {
+                tpTimePicker.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+    private void Edit(WeekDay chosenDay, ArrayList<WeekDay> weekDays) {
+        for(WeekDay day: weekDays){
+            if(day.getDay().equals(chosenDay.getDay())){
+                day.setStartTime(chosenDay.getStartTime());
+                day.setStopTime(chosenDay.getStopTime());
+            }
+        }
+    }
+
+    private void Add(WeekDay chosenDay, ArrayList<WeekDay> weekDays) {
+        weekDays.add(chosenDay);
+    }
+
+    private boolean isDayExisted(WeekDay chosenDay, ArrayList<WeekDay> weekDays) {
+        if(weekDays.contains(chosenDay)){
+            return true;
+        }else{
+            return false;
         }
 
     }
