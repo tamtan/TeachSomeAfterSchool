b35e393a47c5c82225473f13d1f39183981b35d3
Tam Tan <vinhtam92@gmail.com>
2015-10-14 16:59:54 +0700

    - Get avatar from gallery or camera taking.

--- app/src/main/java/com/example/pc/teachsomeafterschool/Student/StudentInfoActivity.java
+++ app/src/main/java/com/example/pc/teachsomeafterschool/Student/StudentInfoActivity.java
@@ -2,9 +2,18 @@ package com.example.pc.teachsomeafterschool.Student;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.Window;
 import android.widget.CheckBox;
@@ -21,11 +30,19 @@ import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by TAM on 06-Oct-15.
  */
 @EActivity(R.layout.student_info)
 public class StudentInfoActivity extends Activity {
+    private static final int REQUEST_LOAD_IMAGE = 1;
+    private static final int REQUEST_IMAGE_CAPTURE = 2;
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 3 ;
     @ViewById
     EditText edtStudent_class, edtStudent_full_name, edtStudent_phone_number, edtStudent_real_class, edtStudent_school, edtStudent_address;
     @ViewById
@@ -33,12 +50,12 @@ public class StudentInfoActivity extends Activity {
     @ViewById
     TextView tvAvatar;
     @ViewById
-    ImageView imgBack, imgOk;
+    ImageView imgBack, imgOk, imgAvatar;
 
     Student studentModel;
     String avatarURL;
-
-
+    ImageRecommendDialog imageDig;
+    Uri imageUri                      = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +68,7 @@ public class StudentInfoActivity extends Activity {
         super.onResume();
         studentModel = new Student();
         cbMale.setChecked(true);
+        imageDig = new ImageRecommendDialog(this);
     }
 
     @Click({R.id.cbFemale, R.id.cbMale, R.id.tvAvatar, R.id.imgBack, R.id.imgOk})
@@ -82,7 +100,7 @@ public class StudentInfoActivity extends Activity {
                 }
                 break;
             case R.id.tvAvatar:
-                ImageRecommendDialog imageDig = new ImageRecommendDialog(this);
+
                 imageDig.show();
         }
     }
@@ -124,17 +142,50 @@ public class StudentInfoActivity extends Activity {
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Toast.makeText(this, "hello", Toast.LENGTH_LONG).show();
+        if (requestCode == REQUEST_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            imgAvatar.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+            imgAvatar.setBackgroundColor(getResources().getColor(R.color.gray));
+            studentModel.setImage_url(picturePath);
+//            imgAvatar.setBackground(getResources().getDrawable(R.drawable.avatar));
+
 //        Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 //        startActivityForResult(i, 1);
+        }
+
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            /*********** Load Captured Image And Data Start ****************/
+
+            String imageId = convertImageUriToFile(imageUri, StudentInfoActivity.this);
+
+
+            //  Create and excecute AsyncTask to load capture image
+
+            new LoadImagesFromSDCard().execute("" + imageId);
+
+            /*********** Load Captured Image And Data End ****************/
+
+
+        } else if (resultCode == RESULT_CANCELED) {
+
+            Toast.makeText(this, " Picture was not taken ", Toast.LENGTH_SHORT).show();
+        } else {
+
+            Toast.makeText(this, " Picture was not taken ", Toast.LENGTH_SHORT).show();
+        }
     }
-
     class ImageRecommendDialog extends Dialog implements View.OnClickListener {
-        private static final int REQUEST_LOAD_IMAGE = 1;
         Context context;
-
         TextView tvGallery, tvCamera;
-
         public ImageRecommendDialog(Context context) {
             super(context);
             this.context = context;
@@ -155,14 +206,170 @@ public class StudentInfoActivity extends Activity {
         @Override
         public void onClick(View v) {
             if (v.getId() == R.id.tvGallery) {
+                imageDig.dismiss();
                 Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(i, REQUEST_LOAD_IMAGE);
-                Toast.makeText(context, "hello", Toast.LENGTH_LONG).show();
-            } else {
+            } else if (v.getId() == R.id.tvCamera) {
+                imageDig.dismiss();
+                String fileName = "Camera_Example.jpg";
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.TITLE, fileName);
+                values.put(MediaStore.Images.Media.DESCRIPTION, "Image capture by camera");
+                imageUri = getContentResolver().insert(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
 
+                /**** EXTERNAL_CONTENT_URI : style URI for the "primary" external storage volume. ****/
+                // Standard Intent action that can be sent to have the camera
+                // application capture an image and return it.
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+
+                intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
             }
         }
+
     }
+    public static String convertImageUriToFile ( Uri imageUri, Activity activity )  {
+        Cursor cursor = null;
+        int imageID = 0;
+        try {
+            /*********** Which columns values want to get *******/
+            String [] proj={
+                    MediaStore.Images.Media.DATA,
+                    MediaStore.Images.Media._ID,
+                    MediaStore.Images.Thumbnails._ID,
+                    MediaStore.Images.ImageColumns.ORIENTATION
+            };
+
+            cursor = activity.managedQuery(
+
+                    imageUri,         //  Get data for specific image URI
+                    proj,             //  Which columns to return
+                    null,             //  WHERE clause; which rows to return (all rows)
+                    null,             //  WHERE clause selection arguments (none)
+                    null              //  Order-by clause (ascending by name)
+
+            );
+            int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID);
+            int columnIndexThumb = cursor.getColumnIndexOrThrow(MediaStore.Images.Thumbnails._ID);
+            int file_ColumnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+            //int orientation_ColumnIndex = cursor.
+            //    getColumnIndexOrThrow(MediaStore.Images.ImageColumns.ORIENTATION);
+
+            int size = cursor.getCount();
+
+            /*******  If size is 0, there are no images on the SD Card. *****/
+
+            if (size == 0) {
+            }
+            else
+            {
+
+                int thumbID = 0;
+                if (cursor.moveToFirst()) {
+
+                    /**************** Captured image details ************/
+
+                    /*****  Used to show image on view in LoadImagesFromSDCard class ******/
+                    imageID     = cursor.getInt(columnIndex);
+
+                    thumbID     = cursor.getInt(columnIndexThumb);
+
+                    String Path = cursor.getString(file_ColumnIndex);
+
+                    //String orientation =  cursor.getString(orientation_ColumnIndex);
+
+                    String CapturedImageDetails = " CapturedImageDetails : \n\n"
+                            +" ImageID :"+imageID+"\n"
+                            +" ThumbID :"+thumbID+"\n"
+                            +" Path :"+Path+"\n";
+
+                    // Show Captured Image detail on activity
+                    //tvAvatar.setText( CapturedImageDetails );
+
+                }
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+
+        // Return Captured Image ImageID ( By this ImageID Image will load from sdcard )
+
+        return ""+imageID;
+    }
+    public class LoadImagesFromSDCard  extends AsyncTask<String, Void, Void> {
+        private ProgressDialog Dialog = new ProgressDialog(StudentInfoActivity.this);
+        Bitmap mBitmap;
+
+        protected void onPreExecute() {
+            Dialog.setMessage(" Loading image from Sdcard..");
+            Dialog.show();
+        }
+        // Call after onPreExecute method
+        protected Void doInBackground(String... urls) {
+            Bitmap bitmap = null;
+            Bitmap newBitmap = null;
+            Uri uri = null;
+
+
+            try {
+
+                /**  Uri.withAppendedPath Method Description
+                 * Parameters
+                 *    baseUri  Uri to append path segment to
+                 *    pathSegment  encoded path segment to append
+                 * Returns
+                 *    a new Uri based on baseUri with the given segment appended to the path
+                 */
+
+                uri = Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "" + urls[0]);
+
+                /**************  Decode an input stream into a bitmap. *********/
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+
+                if (bitmap != null) {
+
+                    /********* Creates a new bitmap, scaled from an existing bitmap. ***********/
+
+                    newBitmap = Bitmap.createScaledBitmap(bitmap, 170, 170, true);
+
+                    bitmap.recycle();
+
+                    if (newBitmap != null) {
+                        mBitmap = newBitmap;
+                    }
+                }
+            } catch (IOException e) {
+                // Error fetching image, try to recover
+
+                /********* Cancel execution of this task. **********/
+                cancel(true);
+            }
+
+            return null;
+        }
+
+
+        protected void onPostExecute(Void unused) {
+            Dialog.dismiss();
+            if(mBitmap != null)
+            {
+                imgAvatar.setImageBitmap(mBitmap);
+            }
+
+        }
+
+
+
+}
+
 }
 
 
