5611e97f4bda64d23c8689564be00977a374850e
TAM <vinhtam92@gmail.com>
2015-10-18 17:14:11 +0700

    - Need to fix student adapter

--- app/src/main/java/com/example/pc/teachsomeafterschool/Model/tuition.java
+++ app/src/main/java/com/example/pc/teachsomeafterschool/Model/tuition.java
@@ -3,122 +3,32 @@ package com.example.pc.teachsomeafterschool.Model;
 /**
  * Created by TAM on 03-Oct-15.
  */
-public class Tuition {
-    int id;
-    boolean jan;
-    boolean feb;
-    boolean mar;
-    boolean apr;
-    boolean may;
-    boolean jun;
-    boolean jul;
-    boolean aug;
-    boolean sep;
-    boolean oct;
-    boolean nov;
-    boolean dec;
-
-    public int getId() {
-        return id;
+public class tuition {
+    public tuition(String month, int isPay){
+        this.month = month;
+        this.isPay = isPay;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public tuition() {
+
     }
 
-    public boolean isJan() {
-        return jan;
+    public String getMonth() {
+        return month;
     }
 
-    public void setJan(boolean jan) {
-        this.jan = jan;
+    public void setMonth(String month) {
+        this.month = month;
     }
 
-    public boolean isFeb() {
-        return feb;
+    public int getIsPay() {
+        return isPay;
     }
 
-    public void setFeb(boolean feb) {
-        this.feb = feb;
+    public void setIsPay(int isPay) {
+        this.isPay = isPay;
     }
 
-    public boolean isMar() {
-        return mar;
-    }
-
-    public void setMar(boolean mar) {
-        this.mar = mar;
-    }
-
-    public boolean isApr() {
-        return apr;
-    }
-
-    public void setApr(boolean apr) {
-        this.apr = apr;
-    }
-
-    public boolean isMay() {
-        return may;
-    }
-
-    public void setMay(boolean may) {
-        this.may = may;
-    }
-
-    public boolean isJun() {
-        return jun;
-    }
-
-    public void setJun(boolean jun) {
-        this.jun = jun;
-    }
-
-    public boolean isJul() {
-        return jul;
-    }
-
-    public void setJul(boolean jul) {
-        this.jul = jul;
-    }
-
-    public boolean isAug() {
-        return aug;
-    }
-
-    public void setAug(boolean aug) {
-        this.aug = aug;
-    }
-
-    public boolean isSep() {
-        return sep;
-    }
-
-    public void setSep(boolean sep) {
-        this.sep = sep;
-    }
-
-    public boolean isOct() {
-        return oct;
-    }
-
-    public void setOct(boolean oct) {
-        this.oct = oct;
-    }
-
-    public boolean isNov() {
-        return nov;
-    }
-
-    public void setNov(boolean nov) {
-        this.nov = nov;
-    }
-
-    public boolean isDec() {
-        return dec;
-    }
-
-    public void setDec(boolean dec) {
-        this.dec = dec;
-    }
+    String month;
+    int isPay;
 }
